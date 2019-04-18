4b96d96e7e8c656c9ee8b464fa007f9e8956785e
Jose Enrique <kszosze@hotmail.com>
2019-04-18 23:54:35 +0200

    First commit

--- backend/src/main/java/fonobook/models/FoneDataRequest.java
+++ backend/src/main/java/fonobook/models/FoneDataRequest.java
@@ -8,6 +8,7 @@ import lombok.Data;
 public class FoneDataRequest {
 
     private final String TOKEN = "d3bd900eb8759047b24a9d3f4c05100eed9b4a9a53260d99";
+    @Builder.Default
     private int limit = 5;
     private String device;
 }
